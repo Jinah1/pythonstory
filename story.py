a93b9c4ff9df85227a6aeb06a8956bf033d6d377
Jacob Carratello <jacobcar.22983@redlandsschools.net>
2018-09-28 14:55:24 +0000

    story

--- story.py
+++ story.py
@@ -10,10 +10,21 @@ travel=input("You are Chad, and you've been invited to a party hosted by the ren
 if (travel== 'Alone' or 'alone'):
     Alone()
     travel=True
+    party=input("You arrive at the party, and are greeted by Arj. Arj leaves you and you find yourself alone. Overwhelmed by an urge to explore, you wonder if doing so will yield positive results. You can 'explore' your curiosity, or you can 'play' party games with Arj.")
+    if party=='explore' or 'Explore':
+        print("You explore your urges, and find yourself drawn to the attic. Once there, a glint in a dark corner of the room catches your eye. You go to investigate, and find a large tuba. Suddenly, an idea invades your thoughts. With a sly grin, you take the tuba, and start heading down stairs.")
+    elif party=='games' or 'play' or 'Play':
+        print("You arrive at the party, and are greeted by Arj. He invites you to play party games with him, and you don't have any reason to argue. You feel like you've missed something, but it's probably not important.")
+    else:
+        print("Perish")
 elif(travel=='Ride' or 'ride'):
     Ride()
     travel=False
-
+    if party=='games' or 'play' or 'Play':
+        print("You arrive at the party, and are greeted by Arj. He invites you to play party games with him, and you don't have any reason to argue. You feel like you've missed something, but it's probably not important.")
+    else:
+        print("Perish")
+    
 
     
     
